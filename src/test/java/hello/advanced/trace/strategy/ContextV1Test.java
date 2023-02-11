a76059f70dd0dd09ea7cec3d9cd4452346108874
Keunoh <rmsdhcjswo1@gmail.com>
2023-02-11 22:31:41 +0900

    Strategy Pattern

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,7 +1,8 @@
 package hello.advanced.trace.strategy;
 
-import hello.advanced.trace.template.code.AbstractTemplate;
-import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +35,18 @@ public class ContextV1Test {
         log.info("resultTime={}", resultTime);
     }
 
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
 
 }
